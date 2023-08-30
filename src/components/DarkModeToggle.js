b5a340b3b050b9e8c39223a4135a4ab086b48187
Denny Tek <denny@objectively.is>
2023-08-30 15:55:50 -0400

    Add dark mode toggle

---
+++ src/components/DarkModeToggle.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
+
+export const DarkModeToggle = () => {
+  const [isDark, setIsDark] = useState(false);
+  useEffect(() => {
+    if (isDark) {
+      document.querySelector(':root').classList.add('dark');
+    } else {
+      document.querySelector(':root').classList.remove('dark');
+    }
+  }, [isDark]); 
+  return (
+    <Toggle
+      checked={isDark}
+      onChange={({ target }) => setIsDark(target.checked)}
+      icons={{ checked: "", unchecked: "" }}
+      aria-label="Dark mode toggle"
+    />
+  );
+};
