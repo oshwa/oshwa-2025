bca6812945230c109e7ff25c88ea934f6b96c89f
Denny Tek <denny@objectively.is>
2023-10-10 16:06:11 -0400

    Add typography and examples

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,86 +1,110 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <>
-      <div className="p-8 pt-0 pb-5">
-        <div className="grid lg:grid-cols-5 md:grid-cols-5">
-          <div className="col-span-3 lg:mr-5 md:mr-5 notched notched--bg">
-            <p>
-              Ad ea duis aliquip do irure. Reprehenderit sit qui culpa laboris
-              tempor sit mollit sint exercitation proident culpa minim Lorem id.
-              Dolore velit sit sunt deserunt duis. Laboris eu irure cupidatat
-              minim eu anim. Tempor excepteur sit nulla quis commodo do anim non
-              aliquip ea in magna reprehenderit consectetur. Magna est officia
-              duis consequat esse tempor velit aute ad laborum consectetur eu
-              mollit occaecat. Excepteur ipsum ut ea ipsum excepteur cupidatat
-              ea nisi occaecat Lorem eu. Qui occaecat qui do incididunt non amet
-              do deserunt reprehenderit. Aute minim elit elit veniam anim elit
-              do enim anim consectetur labore eiusmod.
-            </p>
-          </div>
-          <div className="col-span-2 h-full notched notched--border notched--border--hover">
-            <p>col 2</p>
-            <p>
-              Ad ea duis aliquip do irure. Reprehenderit sit qui culpa laboris
-              tempor sit mollit sint exercitation proident culpa minim Lorem id.
-              Dolore velit sit sunt deserunt duis. Laboris eu irure cupidatat
-              minim eu anim. Tempor excepteur sit nulla quis commodo do anim non
-              aliquip ea in magna reprehenderit consectetur. Magna est officia
-              duis consequat esse tempor velit aute ad laborum consectetur eu
-              mollit occaecat. Excepteur ipsum ut ea ipsum excepteur cupidatat
-              ea nisi occaecat Lorem eu. Qui occaecat qui do incididunt non amet
-              do deserunt reprehenderit. Aute minim elit elit veniam anim elit
-              do enim anim consectetur labore eiusmod.
-            </p>
+import { ArrowLink } from '../components/Link';
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query IndexQuery {
+      contentfulIndex {
+        id
+        title
+        featuredResearch {
+          id
+          publicationDate(formatString: "DD MMMM YYYY")
+          title
+          prettyUrl
+          shortDescription
+        }
+      }
+      allOshwaCertifications {
+        totalCount
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <>
+        <div className="p-8 pt-0 pb-5">
+          <div className="grid lg:grid-cols-5 md:grid-cols-5">
+            <div className="col-span-3 lg:mr-5 md:mr-5 notched notched--bg index-banner">
+              <div className="index-image-container">
+                <StaticImage
+                  className="temp-index-img"
+                  src="../images/example.png"
+                  alt="Temp Oshwa Logo"
+                />
+                <ArrowLink text="About OSHWA" location="/about" />
+              </div>
+            </div>
+            <div className="col-span-2 h-full notched notched--border notched--border--hover featured-box">
+              <p className="featured-box__date">
+                {data.contentfulIndex.featuredResearch.publicationDate}
+              </p>
+              <h2 className="featured-box__title">
+                {data.contentfulIndex.featuredResearch.title}
+              </h2>
+              <p>{data.contentfulIndex.featuredResearch.shortDescription}</p>
+              <ArrowLink
+                text="About OSHWA"
+                location="/about"
+                variant={'isInverted'}
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="p-8 pt-0 pb-5">
-        <div className="grid lg:grid-cols-4 md:grid-cols-3">
-          <div className="lg:col-span-1 md:col-span-1 lg:mr-5 md:mr-5 notched notched--bg yellow">
-            <p>
-              Ad ea duis aliquip do irure. Reprehenderit sit qui culpa laboris
-              tempor sit mollit sint exercitation proident culpa minim Lorem id.
-              Dolore velit sit sunt deserunt duis. Laboris eu irure cupidatat
-              minim eu anim. Tempor excepteur sit nulla quis commodo do anim non
-              aliquip ea in magna reprehenderit consectetur. Magna est officia
-              duis consequat esse tempor velit aute ad laborum consectetur eu
-              mollit occaecat. Excepteur ipsum ut ea ipsum excepteur cupidatat
-              ea nisi occaecat Lorem eu. Qui occaecat qui do incididunt non amet
-              do deserunt reprehenderit. Aute minim elit elit veniam anim elit
-              do enim anim consectetur labore eiusmod.
-            </p>
-          </div>
+        <div className="p-8 pt-0 pb-5">
+          <div className="grid lg:grid-cols-4 md:grid-cols-3">
+            <div className="lg:col-span-1 md:col-span-1 lg:mr-5 md:mr-5 notched notched--bg yellow project-highlight">
+              <p className="project-highlight__count">
+                {data.allOshwaCertifications.totalCount}
+              </p>
+              <p className="project-highlight__text">
+                Certified Open Source Hardware Projects
+              </p>
+              <ArrowLink text="All Certified Projects" location="/projects" />
+            </div>
 
-          <div className="lg:col-span-3 md:col-span-2 h-full notched notched--border">
+            <div className="lg:col-span-3 md:col-span-2 h-full notched notched--border"></div>
           </div>
         </div>
-      </div>
 
-      <div className="p-8 pt-0 pb-5">
-        <div className="grid lg:grid-cols-4 md:grid-cols-4">
-          <Link className="lg:col-span-1 md:col-span-2 lg:mr-5 md:mr-5 notched notched--bg notched--bg--hover green" to="/programs">
-            Programs
-          </Link>
-          <Link className="lg:col-span-1 md:col-span-2 lg:mr-5 notched notched--bg blue" to="/resources">
-            Resources
-          </Link>
-          <Link className="lg:col-span-1 md:col-span-2 lg:mr-5 md:mr-5 notched notched--bg red" to="/events">
-            <h3>Events</h3>
-          </Link>
-          <Link className="lg:col-span-1 md:col-span-2 notched notched--bg orange" to="/community">
-            <h3>Community</h3>
-          </Link>
+        <div className="p-8 pt-0 pb-5">
+          <div className="grid lg:grid-cols-4 md:grid-cols-4 section-card">
+            <Link
+              className="lg:col-span-1 md:col-span-2 lg:mr-5 md:mr-5 notched notched--bg notched--bg--hover green"
+              to="/programs"
+            >
+              <h3 className="section-card__title">Programs</h3>
+            </Link>
+            <Link
+              className="lg:col-span-1 md:col-span-2 lg:mr-5 notched notched--bg blue"
+              to="/resources"
+            >
+              <h3 className="section-card__title">Resources</h3>
+            </Link>
+            <Link
+              className="lg:col-span-1 md:col-span-2 lg:mr-5 md:mr-5 notched notched--bg red"
+              to="/events"
+            >
+              <h3 className="section-card__title">Events</h3>
+            </Link>
+            <Link
+              className="lg:col-span-1 md:col-span-2 notched notched--bg orange"
+              to="/community"
+            >
+              <h3 className="section-card__title">Community</h3>
+            </Link>
+          </div>
         </div>
-      </div>
-    </>
-  </Layout>
-);
+      </>
+    </Layout>
+  );
+};
 
 /**
  * Head export to define metadata for the page
