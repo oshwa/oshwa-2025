235650cad3ba4f512078dc67e9b22bf428b724c2
Denny Tek <denny@objectively.is>
2023-10-11 18:49:15 -0400

    Add people data

--- src/components/PersonContainer.js
+++ src/components/PersonContainer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export const PersonContainer = ({ name, title }) => {
+
+export const PersonContainer = ({ name, title, profileImageUrl }) => {
+
   return (
     <div className="person-container">
-      <div className="person-container__image" style={{ backgroundImage: "url(https://placehold.jp/500x500.png)" }}></div>
+      <div
+        className="person-container__image"
+        style={{ backgroundImage: `url(${profileImageUrl})` }}
+      ></div>
       <p className="person-container__name">{name}</p>
       <p className="person-container__title">{title}</p>
     </div>
