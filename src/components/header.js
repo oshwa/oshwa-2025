667e26b83074729fa99676f5afbcf9aa2e43099d
Ena Bek <enabek@gmail.com>
2023-10-13 20:15:30 +0900

    Add fixed nav

--- src/components/header.js
+++ src/components/header.js
@@ -9,17 +9,16 @@ const Header = ({ active, setActive, handleClick, navType }) => {
   const ref = useRef();
   useOnClickOutside(ref, () => setActive(false));
 
-  const headerClassNames = [`p-8`];
+  const headerClassNames = [`p-8 pb-5`];
 
   return (
     <>
       <header
         ref={ref}
-        className={`${
-          active
-            ? `${classNames(navType, headerClassNames, `nav-is-active`)}`
-            : `${classNames(navType, headerClassNames)}`
-        }`}
+        className={`${active
+          ? `${classNames(navType, headerClassNames, `nav-is-active`)}`
+          : `${classNames(navType, headerClassNames)}`
+          }`}
       >
         <NavBarA handleClick={handleClick} active={active} />
         <MainNavA handleClick={handleClick} active={active} />
