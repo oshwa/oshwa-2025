a17c99a6226e1f161705d18f8cf026fb634455c1
Denny Tek <denny@objectively.is>
2024-01-03 14:03:10 -0500

    Fix localStorage error

--- src/helpers/useColorScheme.js
+++ src/helpers/useColorScheme.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from 'react';
+const windowGlobal = typeof window !== 'undefined' && window;
 
 export function useColorScheme() {
   const [isDark, setIsDark] = useState(() => {
-    const colorScheme = localStorage.getItem('colorScheme');
+    const colorScheme =
+      windowGlobal?.localStorage == null
+        ? null
+        : windowGlobal.localStorage.getItem('colorScheme');
     return colorScheme === null ? true : colorScheme === 'dark';
   });
 
   useEffect(() => {
     if (isDark) {
       document.querySelector(':root').classList.add('dark');
-      localStorage.setItem('colorScheme', 'dark');
+      if (windowGlobal?.localStorage != null) {
+        localStorage.setItem('colorScheme', 'dark');
+      }
     } else {
       document.querySelector(':root').classList.remove('dark');
-      localStorage.setItem('colorScheme', '');
+      if (windowGlobal?.localStorage != null) {
+        localStorage.setItem('colorScheme', '');
+      }
     }
   }, [isDark]);
 
