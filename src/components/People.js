663e44cb1a6931bf3ebb546388e4241b90eda7a5
Ena Bek <enabek@gmail.com>
2024-06-14 14:19:16 -0400

    Add people page using generic page template

---
+++ src/components/People.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+
+import { PersonContainer } from '../components/PersonContainer';
+
+export const PeopleTemplate = () => {
+  const data = useStaticQuery(graphql`
+    query PeoplePageQuery {
+      teamMembers: allContentfulPeople(filter: { type: { eq: "OSHWA Team" } }) {
+        edges {
+          node {
+            id
+            displayName
+            image {
+              id
+              file {
+                url
+                fileName
+                contentType
+              }
+            }
+            prettyUrl
+            title
+            affiliation
+            type
+          }
+        }
+      }
+      boardMembers: allContentfulPeople(
+        filter: { type: { eq: "OSHWA Board Members" } }
+      ) {
+        edges {
+          node {
+            id
+            displayName
+            image {
+              id
+              file {
+                url
+                fileName
+                contentType
+              }
+            }
+            prettyUrl
+            title
+            affiliation
+            type
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <div className="p-10 pt-0 pb-5">
+        <h2 className="generic-heading-2 py-8">Team</h2>
+        <div className="grid lg:grid-cols-4 md:grid-cols-4">
+          {data.teamMembers.edges.map(teamMember => {
+            return (
+              <Link
+                key={teamMember.node.id}
+                className="lg:col-span-1 md:col-span-2 lg:mr-5 md:mr-5 notched notched--border"
+                to={teamMember.node.prettyUrl}
+              >
+                <PersonContainer
+                  name={teamMember.node.displayName}
+                  title={teamMember.node.title}
+                  profileImageUrl={teamMember.node.image.file.url}
+                />
+              </Link>
+            );
+          })}
+        </div>
+
+        <h2 className="generic-heading-2 generic-heading-2--2nd-heading py-8">Board Members</h2>
+        <div className="grid lg:grid-cols-4 md:grid-cols-4">
+          {data.boardMembers.edges.map(boardMember => {
+            return (
+              <Link
+                key={boardMember.node.id}
+                className="lg:col-span-1 md:col-span-2 lg:mr-5 md:mr-5 notched notched--border"
+                to={boardMember.node.prettyUrl}
+              >
+                <PersonContainer
+                  name={boardMember.node.displayName}
+                  title={boardMember.node.title}
+                  profileImageUrl={boardMember.node.image.file.url}
+                />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/templates/generic-page.js
+++ src/templates/generic-page.js
@@ -1,27 +1,42 @@
-// import React from 'react';
-// import { graphql } from 'gatsby';
+import React from 'react';
+import { graphql } from 'gatsby';
 
-// import Layout from '../components/layout';
-// import { GenericHeader } from '../components/GenericHeader';
+import Layout from '../components/layout';
+import { GenericHeader } from '../components/GenericHeader';
+import { PeopleTemplate } from '../components/People';
 
-// export default function ProjectPage({ data }) {
-//   const pageData = data.contentfulGenericPage;
+export default function ProjectPage({ data }) {
+  const pageData = data.contentfulGenericPage;
 
-//   return (
-//     <Layout>
-//       <>
-//         <GenericHeader title={pageData.title} />
-//       </>
-//     </Layout>
-//   );
-// }
+  return (
+    <Layout>
+      <>
+        <GenericHeader
+          title={pageData.title}
+          description={pageData.shortDescription.shortDescription}
+          headerImageUrl={pageData.headerImage.url}
+        />
 
-// export const query = graphql`
-//   query ($id: String!) {
-//     contentfulGenericPage(id: { eq: $id }) {
-//       id
-//       prettyUrl
-//       title
-//     }
-//   }
-// `;
+        {pageData.title === "People" && (
+          <PeopleTemplate />
+        )}
+      </>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query ($id: String!) {
+    contentfulGenericPage(id: { eq: $id }) {
+      id
+      prettyUrl
+      title
+      shortDescription {
+        shortDescription
+      }
+      headerImage {
+        url
+      }
+    }
+  }
+`;
