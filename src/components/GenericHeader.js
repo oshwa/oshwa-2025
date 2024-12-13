af43e7f8a6dc2b00b4b81245919b4461bf27779a
Ena Bek <enabek@gmail.com>
2024-12-13 15:21:17 +0900

    Account for no bg-image in genericHeader

--- src/components/GenericHeader.js
+++ src/components/GenericHeader.js
@@ -5,7 +5,14 @@ export const GenericHeader = ({ title, description, headerImageUrl, program, loc
   return (
     <div className="px-8">
       <div className="grid lg:grid-cols-5 md:grid-cols-5 gap-4">
-        <div className="lg:col-span-3 md:col-span-5 sm:col-span-5 h-full notched notched--bg notched--bg--img header-img-container" style={{ backgroundImage: `url(${headerImageUrl})` }}></div>
+        {headerImageUrl ?
+          <div
+            className="lg:col-span-3 md:col-span-5 sm:col-span-5 h-full notched notched--bg notched--bg--img header-img-container"
+            style={{ backgroundImage: `url(${headerImageUrl})` }}>
+          </div>
+          :
+          <div className="lg:col-span-3 md:col-span-5 sm:col-span-5 h-full notched notched--bg notched--bg--img header-img-container"></div>
+        }
         <div className="generic-header lg:col-span-2 md:col-span-5 sm:col-span-5 h-full notched notched--border">
           {program && (
             <span className="program-title">{program}</span>
